e15b76d29a3d896f5bd5a50a8d3b0ee1ef8eb97c
Fidel Jr. Rillon <fideljr.rillon@benilde.edu.ph>
2021-03-16 00:03:19 +0800

    Partial Focus_Main and Installation of SFPro Font Family in the pubspec.yaml

---
+++ lib/Focus/focus_main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class focusMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(' '),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 10, bottom: 20),
+          child: Align(
+            alignment: Alignment(-0.58, 0.90), 
+            child: Text(
+              "Focus",
+              style: TextStyle(
+                fontFamily: 'SF-Pro-Bold',
+                color: Colors.black,
+                fontSize:38
+              ),
+            ),),),
+            Expanded(
+              child: ListView( 
+                padding: const EdgeInsets.only(left: 32, right: 32, top: 15),
+                children: <Widget>[Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 23),
+                  height:  268, width: 347,
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                    child:
+                      Column(
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Text(
+                                "Pomodoro",
+                                style: TextStyle(
+                                  fontFamily: 'SF-Pro-Bold',
+                                  color: Colors.black,
+                                  fontSize: 30,
+                                  letterSpacing: 1,
+                                ),
+                    ),
+                            ],
+                          ),
+                          ],
+                      ),
+                ),],
+              ),
+            ),
+      ],)
+    );
+  }
+}
