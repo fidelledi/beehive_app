5ec7eb8e6f65ab4232c2081a2760c13bde97f3a2
Eruel Ursua <eruelursua@gmail.com>
2021-03-15 19:53:54 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello World',
+              'Hello World!',
             ),
             Text(
               '$_counter',
