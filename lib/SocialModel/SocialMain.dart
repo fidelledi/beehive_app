7dc8d3bb393f1d85042429fc57ad1179640a1046
salamancamonina <moninafelice.salamanca@benilde.edu.ph>
2021-03-24 18:30:13 +0800

    03.24.21

--- lib/SocialModel/SocialMain.dart
+++ lib/SocialModel/SocialMain.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:beehive_app/Landing%20Page/LandingPage.dart';
+import 'package:beehive_app/SocialModel/message_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -10,15 +11,106 @@ class SocialMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Inbox')
-      ),
-      body: Container(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_rounded),
+          ),
+          elevation: 10,
+          title: Padding(
+            padding: const EdgeInsets.only(left:70),
+            child: Text('The Buzz',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'SF-Pro-Bold',
+                  color: Colors.white
+              ),),
+          ),
+          backgroundColor: Color(0xFFF8CD81),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){},
+            ),
+          ],
+        ),
+        body: ListView.builder
+          (itemCount: chats.length,
+            itemBuilder: (BuildContext context, int index){
+              final Message chat= chats[index];
+              return
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow:[
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,),
+                            ] ),
+                        child: CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage(chat.sender.imageUrl),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.65,
+                        padding: EdgeInsets.only(left: 20,),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(chat.sender.name,
+                                  style:TextStyle(
+                                      fontSize: 16,
+                                      fontFamily: 'SF-Pro-Bold'
+                                  ) ,),
+                                chat.sender.isOnline ?
+                                Container(
+                                  width: 7,
+                                  height: 7,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.green,
+                                  ),
+                                ) :
+                                Container(child: null,),
 
-      ),
-      bottomNavigationBar: bottomNavBar(),
+                                Text(chat.time,
+                                  style: TextStyle(
+                                    fontSize: 11,
+                                    fontFamily: 'SF-Pro-Text',
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.black87,
+                                  ),),
+                              ],
+
+                            ),
+                            SizedBox(height: 10,),
+                            Container(
+                              alignment: Alignment.topLeft,
+                              child: Text( chat.text,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  fontFamily: 'SF-Pro-Text',
+                                  color: Colors.black87,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+            })
     );
   }
 }
-
 
